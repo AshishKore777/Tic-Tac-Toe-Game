3a5034d7af6c72c684f4034888b9225d647f65c6
AshishKore777 <53338065+AshishKore777@users.noreply.github.com>
2020-04-25 01:59:17 +0530

    Third Commit

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -14,24 +14,20 @@ from IPython.display import clear_output
 def print_board(l):
 #to print the board in current condition
 
-    #clears the previous output
-    clear_output()    
-    
-    #prints current condition of the board 
     print("     |     |\n {} | {} | {} \n ____|_____|_____".format(l[0],l[1],l[2]))
     print("     |     |\n {} | {} | {} \n ____|_____|_____".format(l[3],l[4],l[5]))
     print("     |     |\n {} | {} | {} \n     |     |     ".format(l[6],l[7],l[8]))
 
 def winner(win_mark):
 #to print the winner of the game
-
+    
     #checks if player1 is the winner
     if win_mark==" "+play1+" ":
         print("Player 1 is the winner")
         
     #checks if player2 is the winner
     elif win_mark==" "+play2+" ":
-        print("Player 2 is the winner")              
+        print("Player 2 is the winner")
 
 def win_check():
 # to chech if any player has won
@@ -68,6 +64,10 @@ def win_check():
     elif l[2]==l[4]==l[6]:
         winner(l[2])
         return True
+    #checks if it's a tie
+    elif (l.count(" "+play1+" ")+l.count(" "+play2+" "))==len(l):
+        print("Whoops!! It's a Tie ")
+        return True
     #checks if no row, no column, no diagonal have same markers
     else:
         return False
@@ -82,7 +82,7 @@ def take_input(m1,m2):
 
     #takes player1's choice to place the marker
     if win_check()!=True and m1==True:
-        p1_pos=int(input("Player 1 enter the postion to place your marker"))
+        p1_pos=int(input("Player 1 enter the postion to place your marker:"))
         # checks if the position is already taken or not
         if l[p1_pos-1] != " "+play1+" " and l[p1_pos-1] != " "+play2+" ":
             place_marker(p1_pos,play1)
@@ -93,12 +93,11 @@ def take_input(m1,m2):
             m2=False
             print("Place Already used!!")
             take_input(m1,m2)
-            return
     print_board(l)
             
     #takes player2's choice to place the marker
     if win_check()!=True and m2==True:
-        p2_pos=int(input("Player 2 enter the postion to place your marker"))
+        p2_pos=int(input("Player 2 enter the postion to place your marker:"))
         # checks if the position is already taken or not
         if l[p2_pos-1] != " "+play1+" " and l[p2_pos-1] != " "+play2+" ":
             place_marker(p2_pos,play2)
@@ -108,7 +107,6 @@ def take_input(m1,m2):
             m2=True
             print("Place Already used!!")
             take_input(m1,m2)
-            return
 
 
 #initializes the index of the board
@@ -117,15 +115,15 @@ l=[" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 "]
 #prints the empty board with position numbers
 print_board(l)
 
-print("Welcome to the Game!!")
+print("Welcome to the Game!!\nNote: You can press any of the alphabet keys or special symbol keys, and select it as your marker.")
 
 #takes input of the marker symbol of both the users
-play1=input("Player 1 enter your marker either X or O:")
-play2=input("Player 2 enter your marker either X or O:")
+play1=input("Player 1 enter your marker:")
+play2=input("Player 2 enter your marker:")
 while True:
     if play1==play2:
         print("Marker already used!!")
-        play2=input("Player 2 enter your marker either X or O:")
+        play2=input("Player 2 enter your marker:")
     else:
         break
     
@@ -133,6 +131,10 @@ while True:
 # Continously takes position input and checks wheather a player has won
 while win_check()!=True:
     take_input(True,True)
+    
+    #clears the previous output
+    clear_output() 
+    
     print_board(l)
     
     
